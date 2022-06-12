9a49801b3422059512f05c3a3df58c24e3fe6233
sameeksharl <59785217+sameeksharl@users.noreply.github.com>
2022-06-13 00:35:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -20,13 +20,11 @@ famObject.fam_initialize("groupname", y)
 
 # r = ex.fam_create_region("samplename", 10000000, '0o77' , example.RAID5)  # variable parameter probls
 r = ex.fam_create_region(famObject)
-
+print("FAM CREATE REGION")
 print(r)
 
-d = ex.my_fam_allocate(famObject,r)
+d = ex.my_fam_allocate(famObject,r)  #wrapper used
 print(d)
-
-# print(ex.myadd(famObject,d,5,6))
 
 
 
@@ -36,10 +34,13 @@ print('FAM ADD')
 famObject.fam_add(d, 0, 6.7)
 famObject.fam_add(d, 0, 7.6)
 
-res = famObject.fam_fetch_float(d, 0)
+res = famObject.fam_fetch_int32(d, 0)
 print(res)
 
 #_____________________________________________
+
+
+
 
 
 print('FAM SUBTRACT')
@@ -51,30 +52,82 @@ famObject.fam_subtract(d, 0, 9.8)
 res = famObject.fam_fetch_float(d, 0)
 print(res)
 
+print('FAM MAX')
+
+
+famObject.fam_max(d, 0, 6)
+famObject.fam_max(d, 0, 9)
+
+res1 = famObject.fam_fetch_int32(d, 0)
+print(res1)
+
 #______________________________________________
 
+print('FAM AND')
+#FAM AND_____________________________________________
 
-#ex.fam_destory_region(famObject, r)
-#print("DESTROY REGION CALLED FROM WRAPPER")
+famObject.fam_and(d, 0, 6)
+famObject.fam_and(d, 0, 9)
+
+res2 = famObject.fam_fetch_int32(d, 0)
+print(res2)
 
 
-# Doesn't work for now - incompatible function arguments (error)____
+
+
+print('FAM OR')
+#FAM OR_____________________________________________
+
+famObject.fam_or(d, 0, 6)
+famObject.fam_or(d, 0, 4)
+
+res3 = famObject.fam_fetch_int32(d, 0)
+print(res3)
+
+
+
+print('FAM XOR')
+#FAM XOR_____________________________________________
+
+famObject.fam_xor(d, 0, 0)
+famObject.fam_xor(d, 0, 0)
+
+res4 = famObject.fam_fetch_int32(d, 0)
+print(res4)
+
+
+
+print('FAM swap')
+#FAM SWAP_____________________________________________
+
+famObject.fam_xor(d, 0, 1)
+famObject.fam_xor(d, 0, 2)
+
+res5 = famObject.fam_fetch_int32(d, 0)
+res6 = famObject.fam_fetch_int32(d, 0)
+print(res6)
+
+print('FAM cmp swap')
+#FAM SWAP_____________________________________________
+
+famObject.fam_compare_swap(d, 0, 1,2)
+famObject.fam_compare_swap(d, 0, 2,3)
+
+res7 = famObject.fam_fetch_int32(d, 0)
+print(res7)
+
+
+
+
+
+
+
+
 
 famObject.fam_destroy_region(r)
 print("DESTROY REGION CALLED FROM FAM API")
 
 #____________________________________________________________________
-
-#famObject.fam_destory_region(r)
-
-
-#opts = example.Fam_Options("default", "127.0.0.1", "8787", "sockets", "FAM_THREAD_SERIALIZE", "rpc", "memory_server", "FAM_CONTEXT_DEFAULT", "1", "PMIX")
-
-#print("CREATED FAM OPTIONS OBJ:", opts)
-
-
-# Fam add and fam_initialize_example give a segmentation fault when fam_options is created using set_fam_options
-
 
 
 famObject.fam_finalize("groupname")
@@ -82,23 +135,6 @@ print("FINALIZE CALLED FROM FAM API")
 famObject.fam_abort(0)
 print("ABORT CALLED FROM FAM API")
 
-
-
-
-
-
-"""
-print("\nRUNNING FAM INITIALIZE EXAMPLE CODE:")
-
-_ = ex.run_fam_initialize_example()
-
-
-
-print("\nRUNNING FAM ADD EXAMPLE CODE:")
-
-_ = ex.run_fam_add_example()
-
-"""
 
 
 
